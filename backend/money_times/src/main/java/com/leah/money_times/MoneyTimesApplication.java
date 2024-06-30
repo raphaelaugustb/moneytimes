cd57553b2694b5a97f3cdcd5f4062d5324b11470
raphaelaugustb <travassosraphael12@gmail.com>
2024-06-30 16:06:57 -0300

    Initial Commit

---
+++ backend/money_times/src/main/java/com/leah/money_times/MoneyTimesApplication.java
@@ -0,0 +1,13 @@
+package com.leah.money_times;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyTimesApplication.class, args);
+	}
+
+}
---
+++ backend/money_times/src/test/java/com/leah/money_times/MoneyTimesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leah.money_times;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
